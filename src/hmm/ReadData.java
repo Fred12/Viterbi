a6c82d28983b38351a9f681fc8848573adeeffcc
Fred12 <freddyism00@gmail.com>
2015-07-21 12:58:00 +0200

    Usereingaben hinzugefügt

--- src/hmm/ReadData.java
+++ src/hmm/ReadData.java
@@ -1,6 +1,7 @@
 package hmm;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,19 +10,33 @@ import java.util.Scanner;
 public class ReadData {
 	Scanner scanner = new Scanner(System.in); 
 	String zeile = "";
+	File source;
+	int zahl;
+	int i = 0;
+	
+	
 	  
 	public ReadData() throws IOException {
-		System.out.println("Bitte den Namen der Datei (falls im selben Programmverzeichnis), oder den absoluten Pfad zum Dateinamen eingeben  : ");
+		System.out.println("Bitte den Namen der Datei (falls im selben Programmverzeichnis), " + "\n" + "oder den absoluten Pfad zum Dateinamen eingeben (Pfad darf keine Leerzeichen enthalten) : ");
 		String dateiname = scanner.next();
 		
-		FileReader fr = new FileReader(dateiname);		
+		source = new File(dateiname);
+		FileReader fr = new FileReader(source);		
 		BufferedReader br = new BufferedReader(fr);
 		
 		
 		while( (zeile = br.readLine()) != null ) {
-		if (zeile.equals("")) {			  
-			  continue;
-		  }	
+				
+			if (zeile.equals("")) {			  
+				  continue;
+			}	
+			
+			for (char c : zeile.toCharArray()) {
+		        if (Character.isDigit(c)) {
+		            zahl = Character.getNumericValue(c);
+		        }	
+		    }
+	 
 		}
 	}
 }
--- src/hmm/Viterbi.java
+++ src/hmm/Viterbi.java
@@ -1,10 +1,66 @@
 package hmm;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Viterbi {
+	Scanner scanner = new Scanner(System.in); 
+	double q0F;
+	double q0U;
+	double FF;
+	double FU;
+	double UU;
+	double UF;
+	ArrayList<Double> arrayEmissionFair = new ArrayList();
+	ArrayList<Double> arrayEmissionUnfair = new ArrayList();
+	
+	double e;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String[] args) throws IOException {
+		//new ReadData();
+		new Viterbi();
+	}
+	
+	
+	public Viterbi() {
+		/*
+		System.out.println("***Willkommen zu unserem Hidden Markov Model (Fair/Unfair)-Würfel Szenario!***\n");
+		System.out.println("Bitte geben Sie die Übergangswahrscheinlichkeit (reelle Zahl, bzw. Double-Wert) an aus dem Anfangszustands den fairen Wuerfel auszuwählen: ");
+		q0F = scanner.nextDouble();
+		System.out.println("Analog dazu bitte die Wahrscheinlichkeit angeben, aus dem Anfangszustand den Unfairen Würfel zu wählen: ");
+		q0U = scanner.nextDouble();
+		System.out.println("Geben Sie nun die WSK an, vom fairen Wuerfel beim fairen Wuerfel zu bleiben: ");
+		FF = scanner.nextDouble();		
+		System.out.println("Geben Sie nun die WSK an, vom fairen Wuerfel zum unfairen Wuerfel zu wechseln: ");
+		FU = scanner.nextDouble();
+		System.out.println("Geben Sie nun die WSK an, vom unfairen Wuerfel beim unfairen Wuerfel zu bleiben");
+		UU = scanner.nextDouble();
+		System.out.println("Geben Sie nun die WSK an, vom unfairen Wuerfel zum fairen Wuerfel zu wechseln: ");
+		UF = scanner.nextDouble();
+		*/
+		System.out.println("*************************************************************\n");
+		System.out.println("Geben Sie nun nacheinander die Emissionswahrscheinlichkeiten für die Zahlen 1-6 des FAIREN Wuerfels an: ");
+		for (int i = 1; i <= 6; i++) {
+		System.out.println("Zahl " +i+ "= ");
+		
+		e = scanner.nextDouble();
+		
+		arrayEmissionFair.add(e);
+		}
+		System.out.println("Geben Sie nun nacheinander die Emissionswahrscheinlichkeiten für die Zahlen 1-6 des UNFAIREN Wuerfels an: ");
+		for (int i = 1; i <= 6; i++) {
+		System.out.println("Zahl " +i+ "= ");
+		e = scanner.nextDouble();
+		arrayEmissionUnfair.add(e);		
+		}
+		
+		System.out.println(arrayEmissionFair.get(0));
+		System.out.println(arrayEmissionFair.get(5));
+		
+		
+		
 	}
 
 }
